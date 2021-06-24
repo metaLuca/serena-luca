f65dc327cb0e67f9c71778249a4edd25072e9ba0
Marcato Luca <marcato@intre.it>
2021-06-25 01:32:19 +0200

    add api key

--- src/views/Sections/Mappa/Mappa.jsx
+++ src/views/Sections/Mappa/Mappa.jsx
@@ -30,7 +30,7 @@ const Mappa = ({ className = '', frontmatter }) => {
         <SectionHeader header={rootHeader} subheader={rootSubHeader} />
       </Row>
       <MyMapComponent
-          googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${API_KEY}v=3.exp&libraries=geometry,drawing,places`}
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${API_KEY}&v=3.exp&libraries=geometry,drawing,places`}
           loadingElement={<div style={{ height: `100%` }} />}
           containerElement={<div style={{ height: `400px` }} />}
           mapElement={<div style={{ height: `100%` }} />}

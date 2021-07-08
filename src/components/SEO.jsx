2e3b267f0cafebe3f422f652a3b150542d00c852
Marcato Luca <marcato@intre.it>
2021-07-09 01:59:07 +0200

    remove from helmet

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -81,17 +81,7 @@ const SEO = ({ lang, description, meta, keywords, title }) => {
             : [],
         )
         .concat(meta)}
-    >
-      <link
-          rel="stylesheet"
-          href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
-          integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
-          crossOrigin=""
-      />
-      <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
-    integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
-    crossOrigin=""/>
-    </Helmet>
+    />
   );
 };
 
